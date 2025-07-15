e378de0aa89e2cd12f9055626573ecc48e805466
hungitlor123 <hungdz369@gmail.com>
2025-07-15 17:57:44 +0700

    add week

--- frontend-web/src/hooks/useIsomorphicLayoutEffect.ts
+++
@@ -1,5 +0,0 @@
-import { useLayoutEffect, useEffect } from "react";
-
-// Sử dụng useLayoutEffect trên client, useEffect trên server để tránh hydration warnings
-export const useIsomorphicLayoutEffect =
-  typeof window !== "undefined" ? useLayoutEffect : useEffect;
